6e6c27f51caa1880a32e47dc9800d01cb01efbde
Dario <104058513+DarioIEL@users.noreply.github.com>
2023-06-15 17:55:22 +0200

    Add files via upload

---
+++ Lez0-Intro2/js/script.js
@@ -0,0 +1,30 @@
+console.log("Ho collegato lo script esterno");
+
+let nomeStudente = "Dario";
+
+let cognomeStudente = "Mennillo";
+
+let corso = "JAITA93";
+
+let etaStudente = 34;
+
+let presenza = true;
+
+let annoIscrizione = 2023;
+
+// alert("Stai presentando uno studente!");
+
+document.write("<h1>" + nomeStudente + " " + cognomeStudente + " corso: " + corso + "</h1><br>");
+
+document.write("Anno Iscrizione: " + annoIscrizione + "<br>");
+
+document.write("Presenza: " + presenza + "<br>");
+
+
+/**
+ * svolgi il seguente esercizio:
+ * in un file js, collegato alla rispettiva pagina html, dichiara e assegna un valore alle seguenti variabili:
+ * nome, cognome, eta, citta, paese
+ * Una volta assegnati i valori a queste variabili stampale sia nella console sia nella pagina html
+ */
+
