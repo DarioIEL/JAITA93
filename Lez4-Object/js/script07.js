85cb025e95c6edd2e98b73b4835a6d00eb228550
Dario <104058513+DarioIEL@users.noreply.github.com>
2023-07-21 10:45:05 +0200

    Add files via upload

--- Lez4-Object/js/script07.js
+++ Lez4-Object/js/script07.js
@@ -1,5 +1,5 @@
 //creo la funzione costruttore dell'utente
-function User(nome, cognome, alias, eta, email, data){
+function User(nome, cognome, alias, eta, email, data) {
     this.nome = nome;
     this.cognome = cognome;
     this.alias = alias;
@@ -7,9 +7,9 @@ function User(nome, cognome, alias, eta, email, data){
     this.email = email;
     this.data = data;
 
-    this.toString = function(){
+    this.toString = function () {
         let presentazione = `Utente: ${this.nome} ${this.cognome}  - Alias: ${this.alias} - Età: ${this.eta} - Email: ${this.email} - DataRec: ${this.data}`;
-        
+
         return presentazione;
     }
 }
@@ -21,8 +21,10 @@ let btnRec = document.getElementById("btnRec");
 /**
  * Questa funzione si occuperà di recuperare tutti i campi del form e creare l'oggetto User
  */
-function recuperaInfo(){
-    let nome = document.getElementById("nome").value;
+let nome = document.getElementById("nome");
+
+function recuperaInfo() {
+
     let cognome = document.getElementById("cognome").value;
     let alias = document.getElementById("alias").value;
     let email = document.getElementById("email").value;
@@ -30,16 +32,62 @@ function recuperaInfo(){
     let eta = document.getElementById("eta").value;
 
     //controllo semplice da trasformare in un controllo approfondito
-    if(nome == "" || cognome == "" || alias == "" || email == "" || data == "" || eta == ""){
-        alert("Il tuo form non è ben compilato");
-    }else{
-        let mioUser = new User(nome, cognome, alias, eta, email, data);
+    if (controllaCampi(nome.value, cognome, alias, email, data, eta)) {
 
-        elencoUsers.innerHTML += "<li>" + mioUser.toString() + "</li>"
+        let mioUser = new User(nome.value, cognome, alias, eta, email, data);
+        elencoUsers.innerHTML += "<li>" + mioUser.toString() + "</li>";
+        pulisciCampi();
+    }
+}
+
+
+function pulisciCampi() {
+    nome.value = "";
+    // document.getElementById("nome").value = "";
+    document.getElementById("cognome").value = "";
+    document.getElementById("alias").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("data").value = "";
+    document.getElementById("eta").value = "";
+}
+
+let feed = document.getElementById("feed");
+
+function controllaCampi(nome, cognome, alias, email, data, eta) {
+    feed.innerHTML = "";
+
+    if (nome.length > 1 && cognome.length >= 1 && alias.length >= 8 && email != "" && data != "" && Number(eta) > 18) {
+
+        feed.innerHTML = "Dati inviati";
+        return true;
+    } else {
+
+        if (nome == "") {
+            feed.innerHTML += "Il nome non è ben compilato <br>";
+        }
+
+        if (cognome == "") {
+            feed.innerHTML += "Il cognome non è ben compilato <br>";
+        }
+
+        if (alias == "" || alias.length <= 8) {
+            feed.innerHTML += "L'alias non è ben compilato <br>";
+        }
+
+        if (email == "") {
+            feed.innerHTML += "Email vuota <br>";
+        }
+
+        if (data == "") {
+            feed.innerHTML += "Hai dimenticato la data<br>";
+        }
+
+        if (eta == "" || Number(eta) < 18) {
+            feed.innerHTML += "Età non compatibile <br>";
+        }
+        return false;
     }
 }
 
 
 btnRec.addEventListener("click", recuperaInfo);
-
-
