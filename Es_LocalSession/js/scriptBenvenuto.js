660f7cc527b11665603cbb215cb8af6b7c5eb2b1
Dario <104058513+DarioIEL@users.noreply.github.com>
2023-09-05 14:36:10 +0200

    Add files via upload

--- Es_LocalSession/js/scriptBenvenuto.js
+++ Es_LocalSession/js/scriptBenvenuto.js
@@ -26,6 +26,14 @@ btnLogOut.addEventListener("click", logout);
 
 let btnMod = document.querySelector("#btnMod");
 
+function User(nome, cognome, email, username, avatar){
+    this.nome = nome;
+    this.cognome = cognome,
+    this.email = email,
+    this.username = username,
+    this.avatar = avatar
+}
+
 function modificaUser(){
     //1. Mostra il form
     let divModifica = document.querySelector("#divModifica");
@@ -41,6 +49,7 @@ function modificaUser(){
 
     let utenteConnesso = JSON.parse(localStorage.getItem("user"));
 
+    //vado a riempire i campi input del form di modifica con i valori dell'utente connesso preso dalla LocalStorage
     nome.value = utenteConnesso.nome;
     cognome.value = utenteConnesso.cognome;
     email.value = utenteConnesso.email;
@@ -49,3 +58,31 @@ function modificaUser(){
 }
 
 btnMod.addEventListener("click", modificaUser);
+
+let formModifica = document.querySelector("#formModifica");
+
+function salvaModifiche(){
+    let nome = document.querySelector("#nome");
+    let cognome = document.querySelector("#cognome");
+    let email = document.querySelector("#email");
+    let username = document.querySelector("#username");
+    let avatarForm = document.querySelector("#avatarForm");
+
+    let userModificato = new User(nome.value, cognome.value, email.value, username.value, avatarForm.value);
+    let userModificatoJSON = JSON.stringify(userModificato);
+    localStorage.setItem("user", userModificatoJSON);
+}
+
+formModifica.addEventListener("submit", salvaModifiche);
+
+
+function annullaModifiche(){
+    let divModifica = document.querySelector("#divModifica");
+    //aggiungo la classe per nascondere
+    divModifica.classList.add("d-none");
+}
+
+let btnAnnulla = document.querySelector("#btnAnnulla");
+
+btnAnnulla.addEventListener("click", annullaModifiche);
+
